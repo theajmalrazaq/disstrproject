ae4bece0ad57665d4196b11b939c833ef2e5241e
theajmalrazaq <theajmalrazaq@gmail.com>
2024-11-17 18:21:06 +0500

    complete

--- Question1/Question1.cpp
+++ Question1/Question1.cpp
@@ -17,7 +17,7 @@ public:
 		int x;
 		cout << "Enter size of set A: ";
 		cin >> x;
-		while (x <= 3) {
+		while (x < 3) {
 			cout << "Invalid Size It should be > than 3" << endl;
 			cout << "Enter size of set A: ";
 			cin >> x;
@@ -30,7 +30,7 @@ public:
 		int y;
 		cout << "Enter size of set B: ";
 		cin >> y;
-		while (y <= 3) {
+		while (y < 3) {
 			cout << "Invalid Size It should be > than 3" << endl;
 			cout << "Enter size of set B: ";
 			cin >> y;
@@ -53,6 +53,25 @@ public:
 			cin >> setB[i];
 		}
 	}
+	//function to print sets
+	void print() {
+		cout << "Set A: ";
+			cout << "{";
+		for (int i = 0; i < sizeA; i++) {
+			cout << setA[i] << ",";
+		}
+			cout << "}";
+		cout << endl;
+		cout << "Set B: ";
+		cout << "{";
+		for (int i = 0; i < sizeB; i++) {
+			cout << setB[i] << ",";
+		}
+		cout << "}";
+		cout << endl;
+	}
+
+
 	//function to union 
 	void Union() {
 		int* setU = new int[sizeA + sizeB];
@@ -152,11 +171,184 @@ public:
 	}
 
 	//function to calculate num of relations
-	 void  numofrelations() {
+	void  numofrelations() const {
 		int num = sizeA * sizeB;
 		int rel = pow(2, num);
 		cout << "Number of relations between set A and set B is: " << rel << endl;
 	}
+
+	// Function to check if the relation is reflexive
+	void checkreflexive() {
+		int sizeR;
+		cout << "Enter the number of Pairs: ";
+		cin >> sizeR;
+
+	
+		int** setR = new int* [sizeR];
+		for (int i = 0; i < sizeR; i++) {
+			setR[i] = new int[2];
+		}
+
+		
+		cout << "Enter the pairs:" << endl;
+		for (int i = 0; i < sizeR; i++) {
+			cout << "Enter 1st element of Pair " << i + 1 << ": ";
+			cin >> setR[i][0]; 
+			cout << "Enter 2nd element of Pair " << i + 1 << ": ";
+			cin >> setR[i][1]; 
+		}
+
+		
+		bool isReflexive = true;
+		for (int i = 0; i < sizeR; i++) {
+			int element = setR[i][0];
+			bool found = false;
+
+			// Check if (element, element) exists in the relation
+			for (int j = 0; j < sizeR; j++) {
+				if (setR[j][0] == element && setR[j][1] == element) {
+					found = true;
+					break;
+				}
+			}
+
+			if (!found) {
+				isReflexive = false;
+				break;
+			}
+		}
+
+		
+		if (isReflexive) {
+			cout << "The relation is reflexive." << endl;
+		}
+		else {
+			cout << "The relation is not reflexive." << endl;
+		}
+
+		
+		for (int i = 0; i < sizeR; i++) {
+			delete[] setR[i];
+		}
+		delete[] setR;
+	}
+
+
+
+
+
+	// Function to check if the relation is symmetric
+	void checksymmetric() {
+		int sizeR;
+		cout << "Enter the number of Pairs: ";
+		cin >> sizeR;
+
+		
+		int** setR = new int* [sizeR];
+		for (int i = 0; i < sizeR; i++) {
+			setR[i] = new int[2];
+		}
+
+		
+		cout << "Enter the pairs:" << endl;
+		for (int i = 0; i < sizeR; i++) {
+			cout << "Enter 1st element of Pair " << i + 1 << ": ";
+			cin >> setR[i][0]; 
+			cout << "Enter 2nd element of Pair " << i + 1 << ": ";
+			cin >> setR[i][1]; 
+		}
+
+
+
+	
+		bool isSymmetric = true;
+		for (int i = 0; i < sizeR; i++) {
+			bool foundSymmetric = false;
+			for (int j = 0; j < sizeR; j++) {
+				// Check if the reverse pair (b, a) exists for each pair (a, b)
+				if (setR[i][0] == setR[j][1] && setR[i][1] == setR[j][0]) {
+					foundSymmetric = true;
+					break;
+				}
+			}
+			if (!foundSymmetric) {
+				isSymmetric = false;
+				break;
+			}
+		}
+
+		if (isSymmetric) {
+			cout << "The relation is symmetric." << endl;
+		}
+		else {
+			cout << "The relation is not symmetric." << endl;
+		}
+
+	
+		for (int i = 0; i < sizeR; i++) {
+			delete[] setR[i];
+		}
+		delete[] setR;
+	}
+
+
+
+	// Function to check if the relation is antisymmetric
+	void checkantisymmetric() {
+		int sizeR;
+		cout << "Enter the number of Pairs: ";
+		cin >> sizeR;
+
+		
+		int** setR = new int* [sizeR];
+		for (int i = 0; i < sizeR; i++) {
+			setR[i] = new int[2];
+		}
+
+		cout << "Enter the pairs:" << endl;
+		for (int i = 0; i < sizeR; i++) {
+			cout << "Enter 1st element of Pair " << i + 1 << ": ";
+			cin >> setR[i][0]; 
+			cout << "Enter 2nd element of Pair " << i + 1 << ": ";
+			cin >> setR[i][1]; 
+		}
+
+		
+		bool isAntisymmetric = true;
+		for (int i = 0; i < sizeR; i++) {
+			for (int j = 0; j < sizeR; j++) {
+				if (i != j && setR[i][0] == setR[j][1] && setR[i][1] == setR[j][0]) {
+					if (setR[i][0] != setR[i][1]) {
+						isAntisymmetric = false;
+						break;
+					}
+				}
+			}
+			if (!isAntisymmetric) {
+				break;
+			}
+		}
+
+		
+		if (isAntisymmetric) {
+			cout << "The relation is antisymmetric." << endl;
+		}
+		else {
+			cout << "The relation is not antisymmetric." << endl;
+		}
+
+		
+		for (int i = 0; i < sizeR; i++) {
+			delete[] setR[i];
+		}
+		delete[] setR;
+	}
+
+
+
+
+
+
 	 void showmenu() {
 		 int flag = 1;
 		 while (flag) {
@@ -169,6 +361,11 @@ public:
 			 cout << "4. Sub Set B from A (A-B)" << endl;
 			 cout << "5. Sub Set A from B (B-A)" << endl;
 			 cout << "6. Total Num of Relations" << endl;
+			 cout << "7. Check Reflexive" << endl;
+			 cout << "8. Check Symmetric" << endl;
+			 cout << "9. Check Antisymmetric" << endl;
+			 cout << "10. Print Sets" << endl;
+			 cout << "----------------" << endl;
 			 cout << "0. Exit" << endl;
 			 cout << "Enter Your Choice: ";
 			 cin >> opt;
@@ -194,6 +391,18 @@ public:
 				 break;
 			 case 6:
 				 numofrelations(); 
+				 break;
+			 case 7:
+				 checkreflexive();
+				 break;
+			 case 8:
+				 checksymmetric();
+				 break;
+			 case 9:
+				 checkantisymmetric();
+				 break;
+			 case 10:
+				 print();
 				 break;
 			 default:
 				 cout << "Invalid Choice!!" << endl;
